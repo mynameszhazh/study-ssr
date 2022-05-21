d2e157cc1f78124f6152b9323e7dc362de3f7eb0
徐建辉 <1594776259@qq.com>
2022-05-21 23:40:45 +0800

    change test js

--- test/test.js
+++ test/test.js
@@ -1,33 +1,13 @@
-function maxProfit( prices ) {
-    // write code here
-    // if(prices== null) return 0
-    // let len=prices.length
-    // let dp=[]
-    // for(let i=0;i<len;i++){
-    //     dp[i]=[]
-    // }
-    // dp[0][0]=0
-    // dp[0][1]=-prices[0]
-    // for(let i=1;i<len;i++){
-    //     dp[i][0]=Math.max(dp[i-1][0],dp[i-1][1]+prices[i])
-    //     dp[i][1]=Math.max(dp[i-1][1],-prices[i])
-    // }
-    // return dp[len-1][0]
-     // write code here
-     if(prices === null) return 0
-     let len = prices.length
-     let db = []
-     for(let i=0; i<len; i++) {
-         db[i] = []
-     }
-     db[0][0] = 0
-     db[0][1] = -prices[0]
-     for(let i=1; i<len; i++) {
-         db[i][0] = Math.max(db[i-1][0], db[i-1][1]+prices[i])
-         db[i][1] = Math.max(db[i-1][1], -prices[i])
-     }
-     return db[len-1][0]
+function jumpFloor (n) {
+  if (n === 0 || n === 1) return n
+  let a = 1; let b = 1; let c
+  for (let i = 2; i <= n; ++i) {
+    c = a + b
+    a = b
+    b = c
+  }
+  return c
 }
 
-const a= maxProfit([1,4,9,0,7])
+const a = jumpFloor(99)
 console.log(a)
